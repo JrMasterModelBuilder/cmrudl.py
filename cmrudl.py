64508b53dc87257fb6f3c62d964bafa9742deac4
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2019-02-15 01:00:08 -0500

    Version 1.0.4

--- cmrudl.py
+++ cmrudl.py
@@ -27,7 +27,7 @@ except ImportError:
 	from urllib import quote as urllib_quote
 
 __prog__ = 'cmrudl.py'
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __copyright__ = 'Copyright (c) 2019 JrMasterModelBuilder'
 __license__ = 'MPL-2.0'
 
